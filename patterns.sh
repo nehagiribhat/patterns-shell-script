94de088960b418695887e1248d9466731ee8fd15
nehagiribhat <nehagiribhat@gmail.com>
2020-07-20 00:31:12 +0530

    ADDED: Pattern to Validate Password

--- patterns.sh
+++ patterns.sh
@@ -3,12 +3,14 @@ shopt -s extglob
 
 read -p "Enter Your First Name (space) Last Name " firstName lastName;
 read -p "Enter Email Id " emailId;
-read -p "Enter Mobile Number " mobNum;
+read -p "Enter Mobile Number +" mobNum;
+read -p "Enter Password " password;
 
 patFirstName="^[A-Z]{1}[A-Za-z]{2,}$"
 patLastName="^[A-Z]{1}[A-Za-z]{2,}$"
 patEmailId="^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$"
-patMobNumber="^[\+0-9]{2,3}[ ]?[0-9]{10}$"
+patMobNumber="^[0-9]{2,3}[ ]?[0-9]{10}$"
+patPassword="[0-9a-zA-Z]{7,}$"
 
 if [[ $firstName =~ $patFirstName ]]
 then
@@ -37,3 +39,11 @@ then
 else
         echo "Enter Valid Mobile Number"
 fi
+
+if [[ $password =~ $patPassword ]]
+then
+        echo "Password Accepted"
+else
+        echo "Enter Valid Password"
+fi
+
