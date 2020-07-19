b5de345686cbbe8d63f02f68a45368cf33b75282
nehagiribhat <nehagiribhat@gmail.com>
2020-07-20 00:01:03 +0530

    ADDED: Pattern to Validate Last Name

--- patterns.sh
+++ patterns.sh
@@ -1,13 +1,21 @@
-#!/bin/bash -x
+#!/bin/bash 
 shopt -s extglob
 
-read -p "Enter Your First Name " firstName
+read -p "Enter Your First Name (space) Last Name " firstName lastName
 
 patFirstName="^[A-Z]{1}[A-Za-z]{2,}$"
+patLastName="^[A-Z]{1}[A-Za-z]{2,}$"
 
 if [[ $firstName =~ $patFirstName ]]
 then
         echo "First Name Accepted"
 else
         echo "Enter Valid First Name"
+fi
+
+if [[ $lastName =~ $patLastName ]]
+then
+        echo "Last Name Accepted"
+else
+        echo "Enter Valid Last Name"
 fi
