92ecfc6bef063fdd64a741f724fc9cf9d2b4c9cf
nehagiribhat <nehagiribhat@gmail.com>
2020-07-20 00:10:26 +0530

    ADDED: Pattern to Validate Email Id

--- patterns.sh
+++ patterns.sh
@@ -2,9 +2,11 @@
 shopt -s extglob
 
 read -p "Enter Your First Name (space) Last Name " firstName lastName
+read -p "Enter Email Id " emailId
 
 patFirstName="^[A-Z]{1}[A-Za-z]{2,}$"
 patLastName="^[A-Z]{1}[A-Za-z]{2,}$"
+patEmailId="^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$"
 
 if [[ $firstName =~ $patFirstName ]]
 then
@@ -18,4 +20,11 @@ then
         echo "Last Name Accepted"
 else
         echo "Enter Valid Last Name"
+fi
+
+if [[ $emailId =~ $patEmailId ]]
+then
+        echo "Email Id Accepted"
+else
+        echo "Enter Valid Email Id"
 fi
